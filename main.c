487d98ea0058075d05a4185e9623f33d89cfff3a
Alexandr Evstigneev <hurricup@gmail.com>
2022-04-24 18:28:28 +0300

    Screen is now updated with set FPS form event loop

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 static const int WIDTH = 640;
 static const int HEIGHT = 640;
 static const int BALL_SIZE = 10;
+static const Uint32 FPS = 30;
+static const Uint32 FPS_SIZE_MS = 1000 / FPS;
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
@@ -21,19 +23,22 @@ int main() {
     if (!initialize_app()) {
         exit(1);
     }
-
-    update_screen();
     event_loop();
 }
 
 static void
 event_loop() {
     SDL_Event event;
+    Uint32 currentTicks = 0;
     while (true) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
                 return;
             }
+        }
+        if (SDL_GetTicks() - currentTicks > FPS_SIZE_MS) {
+            update_screen();
+            currentTicks = SDL_GetTicks();
         }
     }
 }
