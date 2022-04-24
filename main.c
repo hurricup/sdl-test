39f214b30c770a74cf7dffa30e7bbd196d7f8fe9
Alexandr Evstigneev <hurricup@gmail.com>
2022-04-24 17:53:17 +0300

    Created a renderer, filled with blue

--- main.c
+++ main.c
@@ -10,6 +10,12 @@ int main() {
     }
     SDL_Window *window = SDL_CreateWindow("program", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT,
                                           SDL_WINDOW_SHOWN);
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 0);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
     SDL_Event event;
     while (SDL_WaitEvent(&event)) {
         if (event.type == SDL_QUIT) {
