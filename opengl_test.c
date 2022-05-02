dc5f6dc91132a381fe76622051fa0832650bc18e
Alexandr Evstigneev <hurricup@gmail.com>
2022-05-02 11:09:39 +0300

    Added second texture, osciallting with red one

--- opengl_test.c
+++ opengl_test.c
@@ -108,6 +108,33 @@ update_screen() {
     SDL_GL_SwapWindow(window);
 }
 
+static void load_texture(GLenum texture_unit, const char *filename) {
+    int width, height, channels_number;
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glActiveTexture(texture_unit);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    // configure texture options
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // creating from external file
+    unsigned char *data = stbi_load(filename, &width, &height, &channels_number, 0);
+    if (data == NULL) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Error loading texture");
+    } else {
+        SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION,
+                    "Loaded %s: %u x %u, channels %u", filename, width, height, channels_number);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    stbi_image_free(data);
+}
+
 static void
 initialize_gl() {
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // background color
@@ -144,29 +171,9 @@ initialize_gl() {
                 glGetString(GL_EXTENSIONS)
     );
 
-    unsigned int texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-
-    // configure texture options
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
     // generating texture
-    int width, height, channels_number;
-    const char *filename = "texture1.png";
-    unsigned char *data = stbi_load(filename, &width, &height, &channels_number, 0);
-    if (data == NULL) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Error loading texture");
-    } else {
-        SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION,
-                    "Loaded %s: %u x %u, channels %u", filename, width, height, channels_number);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    stbi_image_free(data);
+    load_texture(GL_TEXTURE0, "texture1.png");
+    load_texture(GL_TEXTURE1, "texture2.png");
 }
 
 static void initialize_data() {
--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,12 +1,13 @@
-#version 410 core
+#version 430 core
 
 layout(location = 0) in vec4 vertex_color;
 layout(location = 1) in vec2 vertex_tex_coord;
 
-uniform sampler2D our_texture;
+layout(binding = 0) uniform sampler2D texture1;
+layout(binding = 1) uniform sampler2D texture2;
 
 out vec4 color;
 
 void main(){
-    color = texture(our_texture, vertex_tex_coord) * vertex_color;
+    color = mix(texture(texture1, vertex_tex_coord), texture(texture2, vertex_tex_coord), vertex_color.x) * vertex_color;
 }
