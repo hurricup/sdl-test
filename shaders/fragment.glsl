cb39218967e16ae96864c1975b9b206b9709c96c
Alexandr Evstigneev <hurricup@gmail.com>
2022-05-08 09:40:50 +0300

    Refactored attenuation computation
    I'm not sure why it has linear and quadratic parts in the lesson. Probably somehow related to the environment attenuation

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -34,16 +34,16 @@ layout(binding = 3) uniform sampler2D specular_map;
 out vec4 color;
 
 void main(){
-    float distance_decay_const = 2000;
+    float attenuation_const_quadratic = 1.0 / 2000.0;
 
     vec3 light_distance_vector = light.position - frag_pos;
     float light_distance = length(light_distance_vector);
-    float light_distance_decay = distance_decay_const / (light_distance * light_distance + distance_decay_const);
+    float light_attenuation = 1 / (light_distance * light_distance * attenuation_const_quadratic + 1.0);
 
     vec3 camera_distance_vector = camera_pos - frag_pos;
     vec3 camera_direction = normalize(camera_distance_vector);
     float camera_distance = length(camera_distance_vector);
-    float camera_distance_decay = distance_decay_const / (camera_distance * camera_distance + distance_decay_const);
+    float camera_attenuation = 1 / (camera_distance * camera_distance * attenuation_const_quadratic + 1.0);
 
     // ambient_color
     vec3 textured_ambient_color = ambient_color * vec3(texture(texture2, tex_coord));
@@ -52,13 +52,13 @@ void main(){
     vec3 norm  = normalize(normals_model * normal);
     vec3 light_direction = normalize(light_distance_vector);
     float diffuse = max(dot(norm, light_direction), 0.0);
-    vec3 diffuse_color = light.diffuse * (diffuse * material.diffuse * vec3(texture(texture2, tex_coord))) * light_distance_decay;
+    vec3 diffuse_color = light.diffuse * (diffuse * material.diffuse * vec3(texture(texture2, tex_coord))) * light_attenuation;
 
     // specular color
     vec3 reflect_direction = reflect(-light_direction, norm);
     float specular = pow(max(dot(camera_direction, reflect_direction), 0.0), material.shininess);
-    vec3 specular_color = light.specular * (specular * material.specular * vec3(texture(specular_map, tex_coord))) * light_distance_decay;
+    vec3 specular_color = light.specular * (specular * material.specular * vec3(texture(specular_map, tex_coord))) * light_attenuation;
 
     vec4 frag_color = vec4(textured_ambient_color + diffuse_color + specular_color, 1.0);
-    color = mix(texture(texture1, tex_coord), texture(texture2, tex_coord), 1) * frag_color * camera_distance_decay;
+    color = mix(texture(texture1, tex_coord), texture(texture2, tex_coord), 1) * frag_color * camera_attenuation;
 }
